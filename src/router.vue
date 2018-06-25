3ac62727b340e0f9f54ae1cc2365a8462396bb47
fmdm <eeddow@gmail.com>
2018-06-25 19:47:27 +0300

    one-stack solution

--- src/router.vue
+++ src/router.vue
@@ -1,37 +1,41 @@
 <template>
-	<golden :selection-enabled="true" ref="layout">
-		<gl-row ref="container" />
+	<golden ref="layout">
+		<gl-stack ref="container" :closable="false" />
 	</golden>
 </template>
 <script lang="ts">
 import golden from './golden.vue'
 import Vue from 'vue'
 import { Watch, Component, Prop } from 'vue-property-decorator'
-import { glRow } from './gl-group'
+import { glStack } from './gl-group'
 
-@Component({components: {golden, glRow}})
+@Component({components: {golden, glStack}})
 export default class router extends Vue {
 	get ci() { return this.container.contentItem(); }
 	get gl() { return this.layout.gl; }
-	get container(): glRow { return <glRow>this.$refs.container; }
+	get container(): glStack { return <glStack>this.$refs.container; }
 	get layout(): golden { return <golden>this.$refs.layout; }
 	@Watch('$route')
 	change(route) {
-		var stack = this.gl.selectedItem;
-		debugger;
-		if(!stack)
-			this.layout.onGlInitialise(()=> {
-				stack = this.ci;//.addChild;
+		this.layout.onGlInitialise(()=> {
+			this.ci.addChild({
+				type: 'component',
+				componentName: 'route',
+				componentState: { route }
 			});
+		});
 	}
 	mounted() {
 		//With immediate: true, the watch is called before $refs are initialised
 		this.change(this.$route);
 		
 		this.gl.registerComponent('route', function(container, state) {
-			/*state.route.comp.childEl
-			container.getElement().append(comp.childEl);*/
-			debugger;
+			var comp = state.route.matched[0].components.default;
+			if('function'=== typeof comp)
+				comp = new comp();
+			else
+				comp = new Vue(comp);
+			comp.$mount(container._element[0]);
 		});
 	}
 }
